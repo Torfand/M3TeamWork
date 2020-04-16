23640321e770e821e9d48122fc8464dfc2774a4d
Torfand <57662236+Torfand@users.noreply.github.com>
2020-04-16 14:49:26 +0200

    x

---
+++ Team/Pomodoro/Pomodoro/Pomodoro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PomodoroEngine;
+
+namespace Pomodoro
+{
+    class Program : INotifyObject
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Console.ReadLine();
+        }
+
+        public Program()
+        {
+            var pomodoro = new PomodoroEngine.Pomodoro(this, 1);
+            pomodoro.Start();
+        }
+        public void Tick(int minutes, int seconds)
+        {
+            Console.Clear();
+            if (seconds == 0 && minutes == 0)
+            {
+                Console.WriteLine("Drit og dra");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine(minutes + " : " + seconds.ToString("00"));
+        }
+    }
+}
